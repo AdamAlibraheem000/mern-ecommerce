c5d2c2aa8f9fbe77416fb521f8e0c23585b6794d
AdamAlibraheem000 <adamalibraheem@hotmail.com>
2022-09-05 15:05:56 -0400

    added detailed product page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 
 
 function App() {
   return (
-    <>
+    <Router>
       <Header/>
       <main className='py-3'>
         <Container>
-        <HomeScreen></HomeScreen>
+          <Routes>
+            <Route path='/' element={<HomeScreen/>}></Route>
+            <Route path="/product/:id" element={<ProductScreen/>}></Route>
+          </Routes>
         </Container>
       </main>
       <Footer/>
-    </>
+    </Router>
   );
 }
 
